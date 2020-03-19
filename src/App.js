da1068ea95b531de7e9d3ba8f5238f7bcdc6efe8
Alejandro <acuellarh@gmail.com>
2020-03-18 22:12:00 -0500

    basic count

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: ""     
+    }
+  }
+
+  render() {
+    return (
+      <div className="App-container">       
+        <input type="text" value={this.state.name} onChange={this.updateName.bind(this)}/>
+        <div className="">       
+          <input type="text" value={this.state.name.length} onChange={this.updateName.bind(this)}/>                        
+        </div>        
+      </div>
+    );
+  }
+
+  updateName(event){
+    this.setState({
+      name: event.target.value
+    });
+  }
+
 }
 
 export default App;
